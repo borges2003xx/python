6b556d1f8178d91fcf27fc607ae8b654a6c9d01f
borges2003xx <borges2003xx@gmail.com>
2015-10-24 12:22:01 +0200

    sistemato gitter

--- gitter.py
+++ gitter.py
@@ -1,15 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
 
-markup_str = requests.get("https://gitter.im/numenta/public/archives/2015/08/19").content
-soup = BeautifulSoup(markup_str, 'html.parser')
+def riporta(giorno):
+    richiesta="https://gitter.im/PX4/VTOL/archives/"+giorno
+    print richiesta
+    markup_str = requests.get(richiesta).content
+    soup = BeautifulSoup(markup_str, 'html.parser')
 
-chat_container = soup.find(id='chat-container')
-chat_items = chat_container.find_all(class_='chat-item')
+    chat_container = soup.find(id='chat-container')
+    chat_items = chat_container.find_all(class_='chat-item')
 
-for chat_item in chat_items:
-    msg = chat_item.find(class_='chat-item__text').get_text().strip()
-    poster = chat_item.find(class_='chat-item__from').get_text().strip()
-    posted_at = chat_item.find(class_='chat-item__time').get_text().strip()
-    print "({}) {}: {}".format(posted_at, poster, msg)
+    for chat_item in chat_items:
+        msg = chat_item.find(class_='chat-item__text').get_text().strip()
+        msg=msg.encode('ascii', 'ignore')
+        poster = chat_item.find(class_='chat-item__from').get_text().strip()
+        poster=poster.encode('ascii', 'ignore')
+        posted_at = chat_item.find(class_='chat-item__time').get_text().strip()
+        posted_at=posted_at.encode('ascii', 'ignore')
+        pulitura= "({}) {}: {}".format(posted_at, poster, msg)
+        
+        print pulitura
+
+
+
+
+
+a = datetime.datetime.today()
+numdays = 100
+dateList = []
+for x in range (0, numdays):
+    t=(a - datetime.timedelta(days = x))
+    t1=t.strftime('%Y/%m/%d')
+    dateList.append(t1)
+
+for x in dateList:
+    riporta(x)
+
+
+
+
+
